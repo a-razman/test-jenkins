5d8eb213c93539e87e58eae59410d8aeb7ab0107
Razgallah <razgallah.ala@gmail.com>
2018-01-12 09:50:44 +0100

    add all files

---
+++ src/main/java/com/aymaxconsulting/testjenkins/HomeClass.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.aymaxconsulting.testjenkins;
+
+/**
+ *
+ * @author Utilisateur
+ */
+public class HomeClass {
+
+    public HomeClass() {
+
+    }
+
+    public HomeClass(String name, String domain) {
+        this.name = name;
+        this.domain = domain;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the domain
+     */
+    public String getDomain() {
+        return domain;
+    }
+
+    /**
+     * @param domain the domain to set
+     */
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+    private String name;
+    private String domain;
+}
